981ec0a7a7763b38d0f9a277335f86eee73dc55c
Michael Schultz <mikewschultz9029@gmail.com>
2023-03-10 11:08:03 -0500

    added title prop to Card1

--- src/components/CardBody.jsx
+++ src/components/CardBody.jsx
@@ -1,10 +1,10 @@
 import Button from './Button'
 
-const CardBody = () =>
+const CardBody = ({ title }) =>
 {
     return (
         <div className="card-body">
-            <h5 className="card-title">Card 1</h5>
+            <h5 className="card-title">{title}</h5>
             <p className="card-text">
                 Some quick example text to build on the card title and make up the
                 bulk of the card's content.
